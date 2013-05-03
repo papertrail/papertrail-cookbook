7e892bc978b3ef702f366a10ab600339adca3a64
Anuj Biyani <abiyani@lytro.com>
2013-05-03 10:56:45 -0700

    add tag to state file name to ensure uniqueness

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -5,12 +5,13 @@ describe 'papertrail-cookbook::default' do
     chef_run = runner('test', {
       papertrail: {
         watch_files: {
-          'test/file/name.jpg' => 'test-file'
+          'test/file/name.jpg' => 'test_file'
         }
       }
     }).converge('papertrail-cookbook::default')
 
     expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileName test/file/name.jpg'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileStateFile state_file_name'
+    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileTag test_file'
+    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileStateFile state_file_name_test_file'
   end
 end
