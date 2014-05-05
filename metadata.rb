3a8d820b7cf522e918bd02c026735bab55e394a1
Mike Heffner <mikeh@fesnel.com>
2014-05-05 14:52:41 -0400

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "mike@librato.com"
 license          "Apache 2.0"
 description      "Installs/Configures Sys Logging to papertrailapp.com"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 name             "papertrail"
 
 depends          "rsyslog"
