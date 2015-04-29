3fbf2b4f16106d83c0f5233d3d127c083e9a5acb
Lars Brillert <lars@railslove.com>
2015-04-29 12:10:43 +0200

    fix spec, see: f3d65229

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -111,6 +111,6 @@ describe 'papertrail-cookbook::default' do
     expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionResumeRetryCount -1'
     expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueMaxDiskSpace 100M'
     expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueSize 100000'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueFileName /var/log/rsyslog_queue_main'
+    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueFileName papertrailqueue'
   end
 end
