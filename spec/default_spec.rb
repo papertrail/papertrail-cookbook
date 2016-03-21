706d9265d7bdc592b955bc9928793b4f112476a4
fh <fh-gitcommit@fholzhauer.de>
2016-03-21 16:18:25 +0100

    Renew test setup, get it up and running
    Tests were broken since at least https://github.com/librato/papertrail-cookbook/commit/f3d6522990f25d667ded1a8dbc1b1ac61aae81b9

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,29 +1,37 @@
 require 'spec_helper'
 
-describe 'papertrail-cookbook::default' do
+describe 'papertrail::default' do
+  let(:runner)   { ChefSpec::SoloRunner.new }
+  let(:chef_run) { runner.converge(described_recipe) }
+  let(:node)     { runner.node }
+
+  before do
+    node.set['papertrail']['watch_files'] = {'test/file/name.jpg' => 'test_file'}
+  end
+
   it 'require rsyslog' do
-    expect(runner.converge('papertrail-cookbook::default')).to include_recipe 'rsyslog'
+    expect(chef_run).to include_recipe 'rsyslog'
   end
 
   it 'uses the basename of the filename as the suffix for state file name' do
-    chef_run = runner('test', {
-      papertrail: {
-        watch_files: {
-          'test/file/name.jpg' => 'test_file'
-        }
-      }
-    }).converge('papertrail-cookbook::default')
-
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileName test/file/name.jpg'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileTag test_file'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/60-watch-files.conf', '$InputFileStateFile state_file_name_test_file'
+    config_filename = '/etc/rsyslog.d/60-watch-files.conf'
+    expect(chef_run).to render_file(config_filename)
+      .with_content('$InputFileName test/file/name.jpg')
+    expect(chef_run).to render_file(config_filename)
+      .with_content('$InputFileTag test_file')
+    expect(chef_run).to render_file(config_filename)
+      .with_content('$InputFileStateFile state_file_name_test_file')
   end
 
   it 'uses attributes to generate configuration' do
-    chef_run = runner.converge('papertrail-cookbook::default')
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionResumeRetryCount -1'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueMaxDiskSpace 100M'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueSize 100000'
-    expect(chef_run).to create_file_with_content '/etc/rsyslog.d/65-papertrail.conf', '$ActionQueueFileName /var/log/rsyslog_queue_main'
+    config_filename = '/etc/rsyslog.d/65-papertrail.conf'
+    expect(chef_run).to render_file(config_filename)
+      .with_content('$ActionResumeRetryCount -1')
+    expect(chef_run).to render_file(config_filename)
+      .with_content('$ActionQueueMaxDiskSpace 100M')
+    expect(chef_run).to render_file(config_filename)
+      .with_content('$ActionQueueSize 100000')
+    expect(chef_run).to render_file(config_filename)
+      .with_content('$ActionQueueFileName papertrailqueue')
   end
 end
